95e6f8b655c65a009c09cd16300f7ef537c28bcd
panjiafeng <happyapan@163.com>
2019-04-12 18:11:12 +0800

    全量期货

--- QiHuoMain.py
+++ QiHuoMain.py
@@ -14,99 +14,98 @@ pre_year_date = tu.pre_year_date()
 today_date = tu.today()
 
 cycle_index = [1, 2, 5, 10, 30, 100, 200]
-#
-# # 猪肉
-# pigDate = PigData("猪肉")
-# pr(analysis.change_rate_day(pigDate.get_data(), cycle_index), pigDate.name)
-#
-# # 鸡蛋
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('JD' + next_month + '.DCE', pre_year_date, today_date, '鸡蛋'),
-#     cycle_index), '鸡蛋' + tu.qi_huo_next_month())
-#
-# # 铁矿石
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('I' + next_month + '.DCE', pre_year_date, today_date, '铁矿石'),
-#     cycle_index), '铁矿石'+tu.qi_huo_next_month())
-#
-# # 乙二醇
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('EG1906.DCE', pre_year_date, today_date,  '乙二醇'),
-#     cycle_index), '乙二醇1906')
-#
-# # 豆一
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('A' + next_month + '.DCE', pre_year_date, today_date,  '豆一'),
-#     cycle_index), '豆一'+tu.qi_huo_next_month())
-#
-# # 豆二
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('B' + next_month + '.DCE', pre_year_date, today_date,  '豆二'),
-#     cycle_index), '豆二'+tu.qi_huo_next_month())
-#
-# # 豆粕
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('M' + next_month + '.DCE', pre_year_date, today_date, '豆粕'),
-#     cycle_index), '豆粕' + tu.qi_huo_next_month())
-#
-# # 豆油
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('Y' + next_month + '.DCE', pre_year_date, today_date, '豆油'),
-#     cycle_index), '豆油' + tu.qi_huo_next_month())
-#
-# # 棕榈油
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('P' + next_month + '.DCE', pre_year_date, today_date, '棕榈油'),
-#     cycle_index), '棕榈油' + tu.qi_huo_next_month())
-#
-# # 玉米
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('C' + next_month + '.DCE', pre_year_date, today_date,  '玉米'),
-#     cycle_index), '玉米'+tu.qi_huo_next_month())
-#
-# # 玉米淀粉
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('C' + next_month + '.DCE', pre_year_date, today_date,  '玉米淀粉'),
-#     cycle_index), '玉米淀粉'+tu.qi_huo_next_month())
-#
-# # 纤维板
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('FB' + next_month + '.DCE', pre_year_date, today_date,  '纤维板'),
-#     cycle_index), '纤维板'+tu.qi_huo_next_month())
-#
-# # 焦炭
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('J' + next_month + '.DCE', pre_year_date, today_date,  '焦炭'),
-#     cycle_index), '焦炭'+tu.qi_huo_next_month())
-#
-# # 焦煤
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('JM' + next_month + '.DCE', pre_year_date, today_date, '焦煤'),
-#     cycle_index), '焦煤' + tu.qi_huo_next_month())
-#
-# # 塑料
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('L' + next_month + '.DCE', pre_year_date, today_date, '塑料'),
-#     cycle_index), '塑料' + tu.qi_huo_next_month())
-#
-# # PVC聚氯乙烯
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('V' + next_month + '.DCE', pre_year_date, today_date, 'PVC聚氯乙烯'),
-#     cycle_index), 'PVC聚氯乙烯' + tu.qi_huo_next_month())
-#
-# # 聚丙烯
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('PP' + next_month + '.DCE', pre_year_date, today_date, '聚丙烯'),
-#     cycle_index), '聚丙烯' + tu.qi_huo_next_month())
-#
-# # 胶合板
-# pr(analysis.change_rate_day(
-#     bd.get_qihuo_simaple('BB' + next_month + '.DCE', pre_year_date, today_date,  '胶合板'),
-#     cycle_index), '胶合板'+tu.qi_huo_next_month())
-#
-#
-# 黄金
-pr(analysis.change_rate_day(mofComData.get_data('281'), cycle_index), mofComData.get_product_name('281'))
 
-# 铝
-pr(analysis.change_rate_day(mofComData.get_data('314'), cycle_index), mofComData.get_product_name('314'))
+# 猪肉
+pigDate = PigData("猪肉")
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(pigDate.get_data(), cycle_index),
+       pigDate.name)
+
+# 鸡蛋
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('JD' + next_month + '.DCE', pre_year_date, today_date, '鸡蛋'),
+    cycle_index), '鸡蛋' + tu.qi_huo_next_month())
+
+# 铁矿石
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('I' + next_month + '.DCE', pre_year_date, today_date, '铁矿石'),
+    cycle_index), '铁矿石' + tu.qi_huo_next_month())
+
+# 乙二醇
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('EG1906.DCE', pre_year_date, today_date, '乙二醇'),
+    cycle_index), '乙二醇1906')
+
+# 豆一
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('A' + next_month + '.DCE', pre_year_date, today_date, '豆一'),
+    cycle_index), '豆一' + tu.qi_huo_next_month())
+
+# 豆二
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('B' + next_month + '.DCE', pre_year_date, today_date, '豆二'),
+    cycle_index), '豆二' + tu.qi_huo_next_month())
+
+# 豆粕
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('M' + next_month + '.DCE', pre_year_date, today_date, '豆粕'),
+    cycle_index), '豆粕' + tu.qi_huo_next_month())
+
+# 豆油
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('Y' + next_month + '.DCE', pre_year_date, today_date, '豆油'),
+    cycle_index), '豆油' + tu.qi_huo_next_month())
+
+# 棕榈油
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('P' + next_month + '.DCE', pre_year_date, today_date, '棕榈油'),
+    cycle_index), '棕榈油' + tu.qi_huo_next_month())
+
+# 玉米
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('C' + next_month + '.DCE', pre_year_date, today_date, '玉米'),
+    cycle_index), '玉米' + tu.qi_huo_next_month())
+
+# 玉米淀粉
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('C' + next_month + '.DCE', pre_year_date, today_date, '玉米淀粉'),
+    cycle_index), '玉米淀粉' + tu.qi_huo_next_month())
+
+# 纤维板
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('FB' + next_month + '.DCE', pre_year_date, today_date, '纤维板'),
+    cycle_index), '纤维板' + tu.qi_huo_next_month())
+
+# 焦炭
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('J' + next_month + '.DCE', pre_year_date, today_date, '焦炭'),
+    cycle_index), '焦炭' + tu.qi_huo_next_month())
+
+# 焦煤
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('JM' + next_month + '.DCE', pre_year_date, today_date, '焦煤'),
+    cycle_index), '焦煤' + tu.qi_huo_next_month())
+
+# 塑料
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('L' + next_month + '.DCE', pre_year_date, today_date, '塑料'),
+    cycle_index), '塑料' + tu.qi_huo_next_month())
+
+# PVC聚氯乙烯
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('V' + next_month + '.DCE', pre_year_date, today_date, 'PVC聚氯乙烯'),
+    cycle_index), 'PVC聚氯乙烯' + tu.qi_huo_next_month())
+
+# 聚丙烯
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('PP' + next_month + '.DCE', pre_year_date, today_date, '聚丙烯'),
+    cycle_index), '聚丙烯' + tu.qi_huo_next_month())
+
+# 胶合板
+p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(
+    bd.get_qihuo_simaple('BB' + next_month + '.DCE', pre_year_date, today_date, '胶合板'),
+    cycle_index), '胶合板' + tu.qi_huo_next_month())
+
+all_qi_huo = mofComData.get_all_product()
+for key in all_qi_huo:
+    p_file('./dataFiles/qihuo' + today_date + '.txt', analysis.change_rate_day(mofComData.get_data(key), cycle_index),
+           all_qi_huo[key])
--- tools/PrintUtil.py
+++ tools/PrintUtil.py
@@ -1,8 +1,24 @@
+import sys
+
+
 def pr(value, title):
     print('-' * 25 + title + '-' * 25)
-    for one in value:
-        print('%s: %10d天 %10.10s%%' % (one[0], one[1], one[2]))
+    if value is not None:
+        for one in value:
+            print('%s: %10d天 %10.10s%%' % (one[0], one[1], one[2]))
     p_line()
+
+
+def p_file(file_path, value, title):
+    savedStdout = sys.stdout  # 保存标准输出流
+    with open(file_path, 'at') as file:
+        sys.stdout = file  # 标准输出重定向至文件
+        print('-' * 25 + title + '-' * 25)
+        if value is not None:
+            for one in value:
+                print('%s: %10d天 %10.10s%%' % (one[0], one[1], one[2]))
+        # p_line()
+    sys.stdout = savedStdout  # 恢复标准输出流
 
 
 def p_line():
